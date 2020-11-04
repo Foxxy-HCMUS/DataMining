4f415196452fa56f98677420729b8b7a5fe0fc40
Seeking <18120212@student.hcmus.edu.vn>
2020-11-04 21:35:40 +0700

    Add header to ExludeDuplicate

--- Source/ExcludeDuplicate.py
+++ Source/ExcludeDuplicate.py
@@ -4,16 +4,10 @@ class ExcludeDuplicate:
     def ExcludeDuplicate(filename):
 
             oldList=file.RowFile.getInstance(filename).data.copy()
-            newList = set(tuple(x) for x in oldList)
-            list(newList)
+            newList = set(tuple(x) for x in oldList[1:])
+            newList=sorted(newList)
+            newList.insert(0,oldList[0])
             return newList
-
-
     pass
 
-
-
-
-
-
-ExcludeDuplicate.ExcludeDuplicate("house-prices.csv")
+print(ExcludeDuplicate.ExcludeDuplicate("house-prices.csv")[0])
