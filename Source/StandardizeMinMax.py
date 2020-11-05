3b75528dabcf4099b1f949d88073e0e31fc82f02
Seeking <18120212@student.hcmus.edu.vn>
2020-11-05 22:57:27 +0700

    Thêm tham số Attribute cần chuẩn hóa vào hàm Min-max, ZSocre

--- Source/StandardizeMinMax.py
+++ Source/StandardizeMinMax.py
@@ -21,7 +21,7 @@ class StandardizeMinMax:
         return (value-min_i)/(max_i-min_i)*(1-0)+0
 
     @staticmethod
-    def StandardizeMinMax(fileIn, fileOut):
+    def StandardizeMinMax(fileIn, fileOut,attrb):
         """chuẩn hoá giá trị số trong file fileIn
         Xuất kết quả ra file fileOut
 
@@ -36,36 +36,52 @@ class StandardizeMinMax:
         # Đọc data theo cột, mỗi cột là 1 attribute
         oldList = file.ColumnFile.getInstance(fileIn).data.copy()
 
+        find=False
         for i in range(len(oldList)):
+            if oldList[i][0]==attrb:
+                find=True
+                break
 
-            # Lấy data cột đó mà không có header
-            newList = oldList[i][1:]
+        if find==False:
+            print("Không tồn tại thuộc tính bạn yêu cầu")
+            return None
 
-            # Kiểm tra xem attb có phải nummeric không
-            isNumber = True
-            j = 0
-            while j in range(len(newList)):
-                if (newList[j] != ''):
-                    if(type(newList[j]) != int and type(newList[j]) != float):
-                        isNumber = False
-                        break
-                else:
-                    newList.pop(j)
-                    j -= 1
-                j += 1
+        # Lấy data cột đó mà không có header
+        newList = oldList[i][1:]
 
-            if len(newList)==0:
-                continue
+        # Kiểm tra xem attb có phải nummeric không
+        isNumber = True
 
-            if isNumber == True:
-                a = min(newList)
-                b = max(newList)
-                for j in range(1, len(oldList[i])):
-                    if oldList[i][j] != '':
-                        oldList[i][j] = StandardizeMinMax.CalculateMinMax(a, b, oldList[i][j])
+        j = 0
+        while j in range(len(newList)):
+            if (newList[j] != ''):
+                if(type(newList[j]) != int and type(newList[j]) != float):
+                    isNumber = False
+                    break
+            else:
+                newList.pop(j)
+                j -= 1
+            j += 1
+
+        if len(newList)==0:
+            print("Thuộc tính bạn yêu cầu không có giá trị")
+            return None
+
+        if isNumber == False:
+            print("Thuộc tính bạn yêu cầu có giá trị không phải số")
+            return None
+
+        if isNumber == True:
+            a = min(newList)
+            b = max(newList)
+            for j in range(1, len(oldList[i])):
+                if oldList[i][j] != '':
+                    oldList[i][j] = StandardizeMinMax.CalculateMinMax(a, b, oldList[i][j])
 
         file.MakeFile.makeColumnFile(fileOut, oldList)
+
+        print("Chuẩn hóa thành công")
         return oldList
 
 
-StandardizeMinMax.StandardizeMinMax('./house-prices.csv', './min-max.csv')
+StandardizeMinMax.StandardizeMinMax('./house-prices.csv', './min-max.csv','MiscFeature')
--- Source/StandardizeZScore.py
+++ Source/StandardizeZScore.py
@@ -2,7 +2,7 @@ import File as file
 
 class StandardizeZScore:
     @staticmethod
-    def StandardizeZScore(fileIn, fileOut):
+    def StandardizeZScore(fileIn, fileOut,attrb):
         """chuẩn hoá dữ liệu dạng số bằng phương pháp z-scores
 
         Args:
@@ -16,44 +16,69 @@ class StandardizeZScore:
         #Đọc data theo cột, mỗi cột là 1 attribute
         oldList = file.ColumnFile.getInstance(fileIn).data.copy()
 
+        find = False
         for i in range(len(oldList)):
-            newList = oldList[i][1:] #Lấy data cột đó mà không có header
+            if oldList[i][0]==attrb:
+                find = True
+                break
 
-            #Kiểm tra xem attb có phải nummeric không
-            isNumber = True
+        if find == False:
+            print("Không tồn tại thuộc tính bạn yêu cầu")
+            return None
 
-            j = 0
-            while j in range(len(newList)):
-                if (newList[j] != ''):
-                    if(type(newList[j]) != int and type(newList[j]) != float):
-                        isNumber = False
-                        break
-                else:
-                    newList.pop(j)
-                    j -= 1
-                j += 1
+        # Lấy data cột đó mà không có header
+        newList = oldList[i][1:]
 
-            if isNumber == True:
-                if len(newList) == 0:
-                    continue
-                #Tính trung bình
-                mean = sum(newList)/len(newList)
+        #Kiểm tra xem attb có phải nummeric không
+        isNumber = True
 
-                #Tính độ lệch chuẩn (n)
-                var = 0
-                for j in range(len(newList)):
-                    var += (newList[j]-mean)**2
-                var = var/(len(newList))
-                var = var**(1/2)
+        j = 0
+        while j in range(len(newList)):
+            if (newList[j] != ''):
+                if(type(newList[j]) != int and type(newList[j]) != float):
+                    isNumber = False
+                    break
+            else:
+                newList.pop(j)
+                j -= 1
+            j += 1
 
-                if var == 0:
-                    continue
-                for j in range(1, len(oldList[i])):
-                    if oldList[i][j] != '':
-                        oldList[i][j] = (oldList[i][j]-mean)/var
+        if len(newList)==0:
+            print("Thuộc tính bạn yêu cầu không có giá trị")
+            return None
+
+        if isNumber == False:
+            print("Thuộc tính bạn yêu cầu có giá trị không phải số")
+            return None
+
+
+
+        #Tính trung bình
+        mean = sum(newList)/len(newList)
+
+        #Tính độ lệch chuẩn (n)
+        var = 0
+        for j in range(len(newList)):
+            var += (newList[j]-mean)**2
+
+        var = var/(len(newList))
+        var = var**(1/2)
+
+        # Nếu độ lệch chuẩn bằng 0 thì tất cả các giá trị trong thuộc tình đều bằng mean
+        if var == 0:
+            for j in range(1, len(oldList[i])):
+                if oldList[i][j] != '':
+                    oldList[i][j] = mean
+            print("Chuẩn hóa thành công")
+            return oldList
+
+        for j in range(1, len(oldList[i])):
+            if oldList[i][j] != '':
+                oldList[i][j] = (oldList[i][j]-mean)/var
 
         file.MakeFile.makeColumnFile(fileOut, oldList)
+        print("Chuẩn hóa thành công")
         return oldList
 
 
-StandardizeZScore.StandardizeZScore('./house-prices.csv', 'z.csv')
+StandardizeZScore.StandardizeZScore('./house-prices.csv', 'z.csv','Street')
